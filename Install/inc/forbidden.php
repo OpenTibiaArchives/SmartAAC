98456e683468585ee45fbb286e85a814fe612569
pekayz <pekayz@e6a30716-9913-0410-bc7f-9f1718357d7f>
2007-04-26 23:58:29 +0000

    * Forgot to commit new files to repos * Main and Manager put in ready to try and port old system to new system xd

---
+++ Install/inc/forbidden.php
@@ -0,0 +1,8 @@
+<?
+echo '
+<p style="font-family: courier;">Access to important parts or the whole installation when it\'s locked is not allowed.<br />
+<b>Administrator</b>: please remove <b>installLock.txt</b> in the Install directory. Or use the admin panel to remove it for you</p>
+';
+
+// logger("Installation attempted");
+?>
